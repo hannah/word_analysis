6a3e7ea4498d27d12d4a4c6a34e6aa09c6563c0b
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-03-15 15:58:06 -0400

    added text to be analyzed and created basic test criteria to be implimented

---
+++ spec/word_analysis_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require_relative '../lib/word_analysis.rb'
+
+describe WordAnalysis do
+
+  it "counts the number of times each word appears" do
+  end
+
+  it "counts the number of times each letter appears" do
+  end
+
+  it "counts the number of times each symbol appears that is not an alphanumeric character" do
+
+  end
+
+  it "displays the three most common words" do
+  end
+
+  it "displays the three most common letters" do
+  end
+
+  it "displays the number of words only used once" do
+  end
+
+  it "displays the number of letters only used once" do
+  end
+
+end
